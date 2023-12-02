a43c7d078cd481ca27952a770ebb946a914d0a8c
Gogasmerter <chebunin19@gmail.com>
2023-12-02 18:15:45 +0400

    Add custom user model

--- SberMenu/SberMenu/settings.py
+++ SberMenu/SberMenu/settings.py
@@ -23,6 +23,8 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
+
+    "users.apps.UsersConfig",
 ]
 
 MIDDLEWARE = [
@@ -66,10 +68,9 @@ DATABASES = {
     },
 }
 
-
+AUTH_USER_MODEL = "users.User"
 # Password validation
 # https://docs.djangoproject.com/en/4.2/ref/settings/#auth-password-validators
-
 AUTH_PASSWORD_VALIDATORS = [
     {
         "NAME": "django.contrib.auth.password_validation.UserAttributeSimilarityValidator",
