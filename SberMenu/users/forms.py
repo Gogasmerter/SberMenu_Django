fdd37dd8351f8e7a5ae053d6fdc2a5039be883c6
Gogasmerter <chebunin19@gmail.com>
2023-12-06 23:13:02 +0400

    Add user signup page

--- SberMenu/users/forms.py
+++ SberMenu/users/forms.py
@@ -7,14 +7,45 @@ from users.models import User
 class CustomAuthenticationForm(AuthenticationForm, BootstrapClassFormMixin):
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
-        self.fields["username"].widget.attrs.update({"class": "form-control email", "placeholder": "Почта", "type": "email"})
+        self.fields["username"].widget.attrs.update(
+            {"class": "form-control email", "placeholder": "Почта", "type": "email"},
+        )
         self.fields["password"].widget.attrs.update({"class": "form-control password", "placeholder": "Пароль"})
 
 
 class CustomUserCreationForm(UserCreationForm, BootstrapClassFormMixin):
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.fields["first_name"].widget.attrs.update(
+            {"class": "form-control email", "type": "text", "placeholder": "Имя", "required": "required"},
+        )
+        self.fields["last_name"].widget.attrs.update(
+            {
+                "class": "form-control email",
+                "type": "text",
+                "placeholder": "Фамилия",
+                "required": "required",
+            },
+        )
+        self.fields["email"].widget.attrs.update(
+            {"class": "form-control email", "type": "email", "placeholder": "E-mail"},
+        )
+        self.fields["password1"].widget.attrs.update(
+            {"class": "form-control password", "type": "password", "placeholder": "Пароль"},
+        )
+        self.fields["password2"].widget.attrs.update(
+            {"class": "form-control password", "type": "password", "placeholder": "Повторите пароль"},
+        )
+
     class Meta:
         model = User
-        fields = ("email",)
+        fields = (
+            "email",
+            "password1",
+            "password2",
+            "first_name",
+            "last_name",
+        )
 
 
 class CustomUserChangeForm(UserChangeForm, BootstrapClassFormMixin):
--- SberMenu/users/views.py
+++ SberMenu/users/views.py
@@ -1,9 +1,9 @@
 from django.views.generic.edit import CreateView
 
-from users.forms import UserCreationForm
+from users.forms import CustomUserCreationForm
 
 
 class RegistrationView(CreateView):
     template_name = "users/signup.html"
-    form_class = UserCreationForm
+    form_class = CustomUserCreationForm
     success_url = "/"
